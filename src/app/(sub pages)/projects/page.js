7c090ec1a20c8787ec37df82ad8ddec444feb1c2
Ye Myat Hein <yemyathein67@gmail.com>
2025-05-18 00:26:18 +0630

    feat: main

--- src/app/(sub pages)/projects/page.js
+++ src/app/(sub pages)/projects/page.js
@@ -15,6 +15,11 @@ export default function Home() {
         className="-z-50 fixed top-0 left-0 w-full h-full object-cover object-center opacity-50"
       /> 
       <ProjectList projects={projectsData} />
+      
+      <p className="text-xs md:text-base mt-2 text-center">And others Company Project.
+        <br/>
+        Currently working at <strong>REI Group</strong> as a frontend developer.
+      </p>
 
       <div className="h-screen fixed top-16 -translate-x-1/2 lg:-translate-x-0 lg:top-20 left-1/2 lg:-left-24 flex justify-center items-center -z-10">
         <RenderModel>
